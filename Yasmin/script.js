4f9955c2b60fca28c96be367815679d16810aab3
Kawã Max <109605145+Kawa-Max@users.noreply.github.com>
2025-10-01 17:08:26 -0300

    photos

--- Yasmin/script.js
+++ Yasmin/script.js
@@ -1,19 +1,19 @@
 const images = [
-  "../photos/1.jpg",
-  "../photos/2.jpg",
-  "../photos/3.jpg",
-  "../photos/4.jpg", 
-  "../photos/5.jpg",
-  "../photos/6.jpg",
-  "../photos/7.jpg",
-  "../photos/8.jpg",
-  "../photos/9.jpg", 
-  "../photos/10.jpg",
-  "../photos/11.jpg",
-  "../photos/12.jpg",
-  "../photos/13.jpg",
-  "../photos/14.jpg",
-  "../photos/15.jpg"
+  "photos/1.jpg",
+  "photos/2.jpg",
+  "photos/3.jpg",
+  "photos/4.jpg", 
+  "photos/5.jpg",
+  "photos/6.jpg",
+  "photos/7.jpg",
+  "photos/8.jpg",
+  "photos/9.jpg", 
+  "photos/10.jpg",
+  "photos/11.jpg",
+  "photos/12.jpg",
+  "photos/13.jpg",
+  "photos/14.jpg",
+  "photos/15.jpg"
   
 
 ];
